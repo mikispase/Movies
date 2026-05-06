a437fc88a9f0f571120ba83f62ed71eddabf61bb
Dimitar Spasovski <dimitar.spasovski@movic.nl>
2026-05-06 13:58:01 +0200

    Initial Commit

---
+++ TwoCodersMovie/TwoCodersMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoCodersMovieApp.swift
+//  TwoCodersMovie
+//
+//  Created by Dimitar Spasovski on 06/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct TwoCodersMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
